de3d5c01f2d98f1ad6ed1b8707e2c76879d7e6de
douglasspgyn <douglasoarespereira@gmail.com>
2017-04-08 15:21:46 -0300

    - Add options to load custom images;

--- app/src/main/java/douglasspgyn/com/github/picassoutils/MainActivity.java
+++ app/src/main/java/douglasspgyn/com/github/picassoutils/MainActivity.java
@@ -1,7 +1,11 @@
 package douglasspgyn.com.github.picassoutils;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import butterknife.Bind;
@@ -29,8 +33,64 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @OnClick(R.id.load_default_image)
-    public void loadImage() {
+    public void loadDefaultImage() {
         PicassoUtils.loadWithThumbnail(this, image, R.drawable.ic_image_black_24dp, thumbnailURL, highQualityURL);
         PicassoUtils.loadAndBlur(this, backgroundImage, R.drawable.background_placeholder, lowQualityURL);
+    }
+
+    @OnClick(R.id.load_custom_image)
+    public void loadCustomImage() {
+        final View dialogLayout = getLayoutInflater().inflate(R.layout.dialog_image_qualities, null);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setView(dialogLayout)
+                .setPositiveButton(getString(R.string.load_image), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                })
+                .setNegativeButton(getString(R.string.cancel), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                });
+
+        final AlertDialog dialog = builder.create();
+        dialog.show();
+
+        final EditText dialogThumbnailURL = ButterKnife.findById(dialogLayout, R.id.dialog_thumbnail_url);
+        final EditText dialogHighQualityURL = ButterKnife.findById(dialogLayout, R.id.dialog_high_quality_url);
+        final EditText dialogBackgroundQualityURL = ButterKnife.findById(dialogLayout, R.id.dialog_background_url);
+        dialog.getButton(DialogInterface.BUTTON_POSITIVE).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean valid = true;
+                dialogThumbnailURL.setError(null);
+                dialogHighQualityURL.setError(null);
+                dialogBackgroundQualityURL.setError(null);
+
+
+                if (dialogThumbnailURL.getText().toString().isEmpty()) {
+                    dialogThumbnailURL.setError(getString(R.string.empty_field));
+                    valid = false;
+                }
+                if (dialogHighQualityURL.getText().toString().isEmpty()) {
+                    dialogHighQualityURL.setError(getString(R.string.empty_field));
+                    valid = false;
+                }
+                if (dialogBackgroundQualityURL.getText().toString().isEmpty()) {
+                    dialogBackgroundQualityURL.setError(getString(R.string.empty_field));
+                    valid = false;
+                }
+
+                if (valid) {
+                    dialog.dismiss();
+                    PicassoUtils.loadWithThumbnail(getBaseContext(), image, R.drawable.ic_image_black_24dp, dialogThumbnailURL.getText().toString(), dialogHighQualityURL.getText().toString());
+                    PicassoUtils.loadAndBlur(getBaseContext(), backgroundImage, R.drawable.background_placeholder, dialogBackgroundQualityURL.getText().toString());
+                }
+            }
+        });
     }
 }
