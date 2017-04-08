06dc1d012945807b34ede60cfd3f1da5f34fc0bf
douglasspgyn <douglasoarespereira@gmail.com>
2017-04-08 14:48:47 -0300

    - Create the Load Default Image to test with default images from the project;

--- app/src/main/java/douglasspgyn/com/github/picassoutils/MainActivity.java
+++ app/src/main/java/douglasspgyn/com/github/picassoutils/MainActivity.java
@@ -2,10 +2,23 @@ package douglasspgyn.com.github.picassoutils;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
+import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
+import douglasspgyn.com.github.picassoutils.utils.PicassoUtils;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String thumbnailURL = "https://raw.githubusercontent.com/douglasspgyn/Picasso-Utils/master/app/src/main/res/drawable/thumbnail.jpg";
+    private static final String lowQualityURL = "https://raw.githubusercontent.com/douglasspgyn/Picasso-Utils/master/app/src/main/res/drawable/low_quality.jpg";
+    private static final String highQualityURL = "https://raw.githubusercontent.com/douglasspgyn/Picasso-Utils/master/app/src/main/res/drawable/high_quality.jpg";
+
+    @Bind(R.id.background_image)
+    ImageView backgroundImage;
+    @Bind(R.id.image)
+    ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +26,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.load_default_image)
+    public void loadImage() {
+        PicassoUtils.loadWithThumbnail(this, image, R.drawable.ic_image_black_24dp, thumbnailURL, highQualityURL);
+        PicassoUtils.loadAndBlur(this, backgroundImage, R.drawable.background_placeholder, lowQualityURL);
     }
 }
